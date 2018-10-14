90b1c8a9b32e6be7b7cae8897ab58109c5a965bb
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-10-14 18:05:02 +0200

    add other appraoch to solve exerice 4 - reverse string

--- src/main/java/Exercise4_ReverseString.java
+++ src/main/java/Exercise4_ReverseString.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Exercise4_ReverseString {
     public static void main(String[] args) {
         System.out.println("Some tests first:");
@@ -7,6 +9,19 @@ public class Exercise4_ReverseString {
         System.out.println(reverse("You know the rules"));
         System.out.println(reverse("And so do I"));
 
+        System.out.println(reverse2("this is a test"));
+        System.out.println(reverse2("I'm so good at java"));
+        System.out.println(reverse2("So good to be bad"));
+        System.out.println(reverse2("You know the rules"));
+        System.out.println(reverse2("And so do I"));
+
+        System.out.println();
+        System.out.println("OK, now give me a line and I'll reverse it");
+
+        Scanner scanner = new Scanner(System.in);
+        String toBeReversed = scanner.nextLine();
+        System.out.println(reverse(toBeReversed));
+        System.out.println(reverse2(toBeReversed));
     }
 
     public static String reverse(String input) {
@@ -16,5 +31,10 @@ public class Exercise4_ReverseString {
             reversed[length - i - 1] = input.charAt(i);
         }
         return new String(reversed);
+    }
+
+    public static String reverse2(String input) {
+        StringBuilder builder = new StringBuilder(input);
+        return builder.reverse().toString();
     }
 }
