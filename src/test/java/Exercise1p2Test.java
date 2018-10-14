3c61bff1626e28e6883135667eb7d1a7673d4213
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-10-14 14:41:47 +0200

    rename - this is not an other version of the same exercies it's 'point 2' of them same exercie

--- src/test/java/Exercise1p2Test.java
+++ src/test/java/Exercise1p2Test.java
@@ -3,14 +3,14 @@ import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class Exercise1v2Test {
+class Exercise1p2Test {
     @org.junit.jupiter.api.Test
     void main() {
         //arrange
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         System.setOut(new PrintStream(baos));
         //act
-        Exercise1v2.main(new String[]{"1", "2"});
+        Exercise1p2.main(new String[]{"1", "2"});
         //assert
         assertEquals("The sum of 1 and 2 is 3", baos.toString().trim());
     }
@@ -21,7 +21,7 @@ class Exercise1v2Test {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         System.setOut(new PrintStream(baos));
         //act
-        Exercise1v2.main(new String[]{"10", "5"});
+        Exercise1p2.main(new String[]{"10", "5"});
         //assert
         assertEquals("The sum of 10 and 5 is 15", baos.toString().trim());
     }
