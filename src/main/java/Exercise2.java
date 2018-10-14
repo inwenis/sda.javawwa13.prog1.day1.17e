146ace494cbbb6192f5d94e2bb0e9e541fe6e602
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-10-14 14:40:27 +0200

    fix exercies 2 - sum 3 and 5 till n

--- src/main/java/Exercise2.java
+++ src/main/java/Exercise2.java
@@ -1,12 +1,12 @@
 public class Exercise2 {
     public static void main(String args[]) {
-        int sum = sum3And5Till();
+        int sum = sum3And5Till(5);
         System.out.print(sum);
     }
 
-    public static int sum3And5Till() {
+    public static int sum3And5Till(int n) {
         int sum = 0;
-        for(int i = 0; i < 10 ; i++) {
+        for(int i = 0; i <= n ; i++) {
             if(i % 3 == 0) {
                 sum += i;
             }
