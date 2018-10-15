90f99ec3e758686d0c2d15df22904a885ee56af6
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-10-15 22:22:14 +0200

    implement exercise 9p1 - draw a square with any length

--- src/main/java/Exercise9.java
+++ src/main/java/Exercise9.java
@@ -1,7 +1,31 @@
 public class Exercise9 {
     public static void main(String[] args) {
+
         System.out.println("***");
         System.out.println("* *");
         System.out.println("***");
+
+        System.out.println(asteriskSquare(3));
+        System.out.println(asteriskSquare(4));
+    }
+
+    public static String asteriskSquare(int length) {
+        String result = "";
+        for (int i = 0; i < length; i++) {
+            result += "*";
+        }
+        result += "\n";
+        for (int i = 0; i < length - 2; i++) {
+            result += "*";
+            for (int j = 0; j < length - 2; j++) {
+                result += " ";
+            }
+            result += "*";
+            result += "\n";
+        }
+        for (int i = 0; i < length; i++) {
+            result += "*";
+        }
+        return result;
     }
 }
