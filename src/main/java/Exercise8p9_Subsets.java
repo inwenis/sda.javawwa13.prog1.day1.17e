ebdd5f408c58ec12204b295d0c05619f8409959b
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-10-15 10:31:17 +0200

    exercise 8p10 - allow to find subset which sums is closest to any given number

--- src/main/java/Exercise8p9_Subsets.java
+++ src/main/java/Exercise8p9_Subsets.java
@@ -2,40 +2,37 @@ import java.util.*;
 
 public class Exercise8p9_Subsets {
     public static void main(String[] args) {
-        findClosestSubSet(new int[]{1,2});
-        findClosestSubSet(new int[]{1,2,3});
-        findClosestSubSet(new int[]{1,2,3,4});
-        findClosestSubSet(new int[]{1,2,3,4,5});
-        findClosestSubSet(new int[]{1,2,3,4,5,6});
-        findClosestSubSet(new int[]{1,2,3,4,5,6,7});
-        findClosestSubSet(new int[]{1,2,3,4,5,6,7,8});
-        findClosestSubSet(new int[]{1,2,3,10,11,20,33,43,123});
-        findClosestSubSet(new int[]{1,2,3,23,11,20,33,43,123,5,6,7});
-        findClosestSubSet(new int[]{1,2,3,23,11,20,33,43,123,5,6,7});
-        findClosestSubSet(new int[]{4,1,100,23,11,20,33,43,123,7});
+        findClosestSubSet(10, new int[]{1,2});
+        findClosestSubSet(10, new int[]{1,2,3});
+        findClosestSubSet(10, new int[]{1,2,3,4});
+        findClosestSubSet(10, new int[]{1,2,3,4,5});
+        findClosestSubSet(10, new int[]{1,2,3,4,5,6});
+        findClosestSubSet(10, new int[]{1,2,3,4,5,6,7});
+        findClosestSubSet(10, new int[]{1,2,3,4,5,6,7,8});
+        findClosestSubSet(10, new int[]{1,2,3,10,11,20,33,43,123});
+        findClosestSubSet(10, new int[]{1,2,3,23,11,20,33,43,123,5,6,7});
+        findClosestSubSet(37, new int[]{1,2,3,23,11,20,33,43,123,5,6,7});
+        findClosestSubSet(23, new int[]{4,1,100,23,11,20,33,43,123,7});
     }
 
-    private static void findClosestSubSet(int[] input) {
+    private static void findClosestSubSet(int n, int[] input) {
         List<List<Integer>> subsets = getSubSet(input);
-
-        System.out.print("closest: ");
-
         List<Integer> closest = subsets
                 .stream()
-                .sorted(Comparator.comparingInt(x -> Math.abs(10 - x.stream().mapToInt(y -> y).sum())))
+                .sorted(Comparator.comparingInt(x -> Math.abs(n - x.stream().mapToInt(y -> y).sum())))
                 .findFirst()
                 .get();
-        System.out.print(closest.stream().mapToInt(y -> y).sum());
-        System.out.print(" ");
-        System.out.println(printSet(closest));
+        int sumOfClosestSubset = closest.stream().mapToInt(y -> y).sum();
+        System.out.print("n=" + n + " sum(subset)=" + sumOfClosestSubset);
+        System.out.println(" subset=" + setToString(closest));
     }
 
-    private static String printSet(List<Integer> subset) {
+    private static String setToString(List<Integer> subset) {
         StringBuilder sb = new StringBuilder();
         sb.append("[");
         for (int i : subset) {
             sb.append(i);
-            sb.append(" ,");
+            sb.append(", ");
         }
         sb.deleteCharAt(sb.length() - 1);
         sb.deleteCharAt(sb.length() - 1);
@@ -45,8 +42,8 @@ public class Exercise8p9_Subsets {
 
     private static List<List<Integer>> getSubSet(int[] elements) {
         List<List<Integer>> allSubsets = new ArrayList<>();
-        double pow = Math.pow(2, elements.length);
-        for (int i = 1; i <= pow - 1; i++) {
+        double superSetCount = Math.pow(2, elements.length) - 1;
+        for (int i = 1; i <= superSetCount; i++) {
 
             List<Integer> currentSubSet = new ArrayList<>();
             for (int k = 0; k < elements.length; k++) {
