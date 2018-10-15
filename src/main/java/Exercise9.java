8d38d721d1cbf1d6c2274515e3b1c61dba0f7794
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-10-15 22:17:05 +0200

    implement exercise 9 - draw a square from stars, length=3

---
+++ src/main/java/Exercise9.java
@@ -0,0 +1,7 @@
+public class Exercise9 {
+    public static void main(String[] args) {
+        System.out.println("***");
+        System.out.println("* *");
+        System.out.println("***");
+    }
+}
