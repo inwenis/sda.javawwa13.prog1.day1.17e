79bc132eeb0ec54de1370cd0fdfc8b96e25700e1
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-10-16 20:03:49 +0200

    implement exercise 12 - number to binary format

---
+++ src/main/java/Exercise12_Binary.java
@@ -0,0 +1,24 @@
+public class Exercise12_Binary {
+    public static void main(String[] args) {
+        System.out.println(toBinary(1));
+        System.out.println(toBinary(2));
+        System.out.println(toBinary(3));
+        System.out.println(toBinary(4));
+        System.out.println(toBinary(5));
+        System.out.println(toBinary(2147483647));
+    }
+
+    private static String toBinary(int number) {
+        String result = "";
+        for (int i = 0; i < 32; i++) {
+            if(number % 2 == 1) {
+                result = "1" + result;
+            } else {
+                result = "0" + result;
+            }
+            number = number/2;
+        }
+
+        return result;
+    }
+}
